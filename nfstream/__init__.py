50fd0ab97f77a6476bcdb31ad4407dde56a75c85
aouinizied <aouinizied@gmail.com>
2019-10-18 17:31:23 +0200

    Implement nfstream module.

---
+++ nfstream/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from nfstream import streamer
+
+"""
+    streamer module is the core module of nfstream package.
+"""
+
+__author__ = """Zied Aouini"""
+__email__ = 'aouinizied@gmail.com'
+__version__ = '0.0.0'
