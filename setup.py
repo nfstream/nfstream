11dd9a4f12a0011f81a35281e7f23fd74be40b94
aouinizied <aouinizied@gmail.com>
2019-11-02 03:50:38 +0100

    Bump version: 1.1.3 → 1.1.4

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ except ImportError:
 
 setup(
     name="nfstream",
-    version='1.1.3',
+    version='1.1.4',
     url='https://github.com/aouinizied/nfstream.git',
     license='LGPLv3',
     description="A flexible and powerful network data analysis library",
