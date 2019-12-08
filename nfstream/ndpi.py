0d940932c4d39d580d5a218cebf21eeb31a524c8
aouinizied <aouinizied@gmail.com>
2019-12-08 08:14:58 +0100

    Revert changes.

--- nfstream/ndpi.py
+++ nfstream/ndpi.py
@@ -1215,10 +1215,7 @@ class NDPI():
     def __init__(self, libpath=None, max_tcp_dissections=10, max_udp_dissections=16):
         self._ffi = cffi.FFI()
         if libpath is None:
-            if "win" in sys.platform[:3]:
-                self._ndpi = self._ffi.dlopen(dirname(abspath(__file__)) + '/libs/libndpi.a')
-            else:
-                self._ndpi = self._ffi.dlopen(dirname(abspath(__file__)) + '/libs/libndpi.so')
+            self._ndpi = self._ffi.dlopen(dirname(abspath(__file__)) + '/libs/libndpi.so')
         else:
             self._ndpi = self._ffi.dlopen(libpath)
         self._ffi.cdef(cc)
