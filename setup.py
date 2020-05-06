0663b84fd28a3b75aa2c8a035ef363aa13bbdeaa
aouinizied <aouinizied@gmail.com>
2020-05-07 01:55:22 +0200

    Bump version: 5.0.1 → 5.1.0

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ cmdclass = {'nDPI': BuildNdpiCommand,
 
 setup(
     name="nfstream",
-    version='5.0.1',
+    version='5.1.0',
     url='https://nfstream.github.io/',
     license='LGPLv3',
     description="A Flexible Network Data Analysis Framework",
