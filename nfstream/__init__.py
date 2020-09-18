028f134052bbf106580b08c4a0acf4b53f806c77
aouinizied <aouinizied@gmail.com>
2020-09-18 19:31:58 +0200

    Bump version: 6.1.2 → 6.1.3

--- nfstream/__init__.py
+++ nfstream/__init__.py
@@ -24,4 +24,4 @@ from .plugin import NFPlugin
 # streamer module is the core module of nfstream package.
 __author__ = """Zied Aouini"""
 __email__ = 'aouinizied@gmail.com'
-__version__ = '6.1.2'
+__version__ = '6.1.3'
--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ cmdclass = {'build_native': BuildNativeCommand,
 
 setup(
     name="nfstream",
-    version='6.1.2',
+    version='6.1.3',
     url='https://www.nfstream.org/',
     license='LGPLv3',
     description="A Flexible Network Data Analysis Framework",
