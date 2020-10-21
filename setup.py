861025f744493db0313627c5c7f21737891c42e3
aouinizied <aouinizied@gmail.com>
2020-10-21 02:17:27 +0200

    Bump version: 6.1.3 → 6.2.0

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ cmdclass = {'build_native': BuildNativeCommand,
 
 setup(
     name="nfstream",
-    version='6.1.3',
+    version='6.2.0',
     url='https://www.nfstream.org/',
     license='LGPLv3',
     description="A Flexible Network Data Analysis Framework",
