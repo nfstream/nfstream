43e736105b03188f53ecbe131e941d3d0e7ddd4a
aouinizied <aouinizied@gmail.com>
2021-09-02 13:00:23 +0200

    Fix numpy build on macos.

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ class BuildNativeCommand(build_ext):
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 python_requires = '>=3.6'
+# Change hash by this line.
 install_requires = ['cffi>=1.14.6',
                     'numpy>=1.19.5',
                     'pandas>=1.1.5',
