037ab5ded51a1612e35aa8b8da78ffd72b0b09de
Zied Aouini <aouinizied@gmail.com>
2022-01-07 15:52:06 +0100

    Update Copyright.

--- examples/flow_printer.py
+++ examples/flow_printer.py
@@ -1,7 +1,7 @@
 """
 ------------------------------------------------------------------------------------------------------------------------
 flow_printer.py
-Copyright (C) 2019-21 - NFStream Developers
+Copyright (C) 2019-22 - NFStream Developers
 This file is part of NFStream, a Flexible Network Data Analysis Framework (https://www.nfstream.org/).
 NFStream is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
