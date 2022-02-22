36e1ff002c6a63b47aedc22b6b649416e10c7e05
Zied Aouini <aouinizied@gmail.com>
2022-02-22 19:31:51 +0100

    Bump version: 6.4.2 → 6.4.3

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ cmdclass = {'build_native': BuildNativeCommand,
 
 setup(
     name="nfstream",
-    version='6.4.2',
+    version='6.4.3',
     url='https://www.nfstream.org/',
     license='LGPLv3',
     description="A Flexible Network Data Analysis Framework",
