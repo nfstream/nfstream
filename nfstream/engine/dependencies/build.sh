df8f54f43cc67bf81c0449c41f3d0ebb4a63ea88
aouinizied <aouinizied@gmail.com>
2022-03-25 15:17:07 +0100

    [WIP] Improve CI.

---
+++ nfstream/engine/dependencies/build.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+#-----------------------------------------------------------------------------------------------------------------------
+# build.sh
+# Copyright (C) 2019-22 - NFStream Developers
+# This file is part of NFStream, a Flexible Network Data Analysis Framework (https://www.nfstream.org/).
+# NFStream is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+# NFStream is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public License along with NFStream.
+# If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------------------------------------------------
+
+build_libpcap() {
+  echo ""
+  echo "---------------------------------------------------------------------------------------------------------------"
+  echo "Compiling libpcap (fanout)"
+  echo "---------------------------------------------------------------------------------------------------------------"
+  cd libpcap
+  ./configure --enable-ipv6 --disable-universal --enable-dbus=no --without-libnl
+  make
+  cd ..
+  echo "---------------------------------------------------------------------------------------------------------------"
+  echo ""
+  }
+
+build_libgpgerror() {
+  echo ""
+  echo "---------------------------------------------------------------------------------------------------------------"
+  echo "Compiling libgpg-error"
+  echo "---------------------------------------------------------------------------------------------------------------"
+  cd libgpg-error
+  ./autogen.sh
+  ./configure -enable-maintainer-mode --enable-static --enable-shared --with-pic --disable-doc --disable-nls
+  cd ..
+  echo "---------------------------------------------------------------------------------------------------------------"
+  echo ""
+  }
+
+build_libgcrypt() {
+  echo ""
+  echo "---------------------------------------------------------------------------------------------------------------"
+  echo "Compiling libgcrypt"
+  echo "---------------------------------------------------------------------------------------------------------------"
+  cd libgcrypt
+  ./autogen.sh
+  ./configure -enable-maintainer-mode --enable-static --enable-shared --with-pic --disable-doc
+  make
+  cd ..
+  echo "---------------------------------------------------------------------------------------------------------------"
+  echo ""
+  }
+
+build_libndpi() {
+  echo ""
+  echo "---------------------------------------------------------------------------------------------------------------"
+  echo "Compiling libndpi"
+  echo "---------------------------------------------------------------------------------------------------------------"
+  cd nDPI
+  ./autogen.sh --with-local-libgcrypt
+  ./configure
+  make
+  cd ..
+  echo "---------------------------------------------------------------------------------------------------------------"
+  echo ""
+  }
+
+
+cd nfstream/engine/dependencies
+build_libpcap
+build_libgpgerror
+build_libgcrypt
+
+
+
+
