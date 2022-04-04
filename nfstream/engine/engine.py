d36899cf4c79d6c0ca77110507412941b07827a9
aouinizied <aouinizied@gmail.com>
2022-04-05 00:29:07 +0200

    Fix OSX CI.

--- nfstream/engine/engine.py
+++ nfstream/engine/engine.py
@@ -14,9 +14,9 @@ If not, see <http://www.gnu.org/licenses/>.
 """
 
 from psutil import net_if_addrs
-import sys
-from .engine_build import ENGINE_PATH
-sys.path.insert(0, ENGINE_PATH)
+import pathlib
+import os
+os.environ['DYLD_LIBRARY_PATH'] = str(pathlib.Path(__file__).parent.resolve())
 from _engine import ffi, lib
 
 
