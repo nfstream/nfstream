fd2404b8d92fef5545a91cbac7c1366cffc5dc3a
Zied Aouini <zied.aouini@datadoghq.com>
2025-11-16 19:45:25 +0100

    fix versions

--- nfstream/engine/lib_engine.c
+++ nfstream/engine/lib_engine.c
@@ -1746,7 +1746,7 @@ void meter_free_flow(struct nf_flow *flow, uint8_t n_dissections, uint16_t splt,
  * engine_version: return engine library version.
  */
 const char *engine_lib_version(void) {
-  return "6.5.5";
+  return "6.5.4";
 }
 
 /**
